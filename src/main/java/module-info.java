25295c492cf368997406bc388c2238f14ccf70ab
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2023-06-07 16:00:27 +0200

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module dev.rapizz {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens dev.rapizz to javafx.fxml;
+    exports dev.rapizz;
+}
