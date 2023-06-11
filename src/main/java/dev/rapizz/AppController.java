d9aa496e72c35ade51639a0be6ffeae47606a70a
Xiumin-Lin <60708580+Xiumin-Lin@users.noreply.github.com>
2023-06-12 00:38:20 +0200

    Add connection fxml

--- src/main/java/dev/rapizz/AppController.java
+++ src/main/java/dev/rapizz/AppController.java
@@ -1,18 +1,11 @@
 package dev.rapizz;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.paint.Color;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+import java.io.IOException;
 
 public class AppController {
 
@@ -27,7 +20,8 @@ public class AppController {
     @FXML
     private Label connStateLabel;
     @FXML
-    private TableView<String> mainTableView;
+    private BorderPane mainViewPane;
+
 
     @FXML
     protected void onMenuButtonClick() {
@@ -46,25 +40,19 @@ public class AppController {
 
     @FXML
     protected void onTestButtonClick() {
-        Utils.Log.info("Click on Test");
+        Utils.Log.info("Click on Test Connection");
         try {
-            ResultSet rs = Database.query("SHOW DATABASES;");
-            if(rs == null) {
-                connStateLabel.setText("No results !");
-                connStateLabel.setTextFill(Color.ORANGE);
-                mainTableView.setPlaceholder(new Label("No rows to display"));
-                return;
-            }
-            connStateLabel.setText("Connected !");
-            connStateLabel.setTextFill(Color.GREEN);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("connection.fxml"));
+            loader.setController(new ConnectionController(connStateLabel)); // set new controller instance
 
-            Utils.populateTableView(mainTableView, rs);
-        } catch (SQLException e) {
-            Utils.Log.error("onTestButtonClick :", e);
-            connStateLabel.setText("Failed !");
-            connStateLabel.setTextFill(Color.RED);
-        } finally {
-            Database.disconnect();
+            ScrollPane connectionPane = loader.load();
+            connectionPane.setFitToHeight(true);
+
+            mainViewPane.setCenter(connectionPane);
+        } catch (IOException e) {
+            Utils.Log.error("Fail when loading connection.fxml :" + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
